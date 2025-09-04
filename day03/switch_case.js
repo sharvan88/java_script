e1cba7060ba7a124d59d0fea8bd8a53056c4bc67
Sharvan <sharvan251120@gmail.com>
2025-09-04 13:14:23 +0530

    solve traafic question using switch case

--- day03/switch_case.js
+++ day03/switch_case.js
@@ -15,31 +15,58 @@ console.log("inavlid day")
 
 // / FIND the current day based on numbers
 
-let day = 5;
-switch (day) {
-  case 1:
-    console.log("monday");
-    break;
-  case 2:
-    console.log("tuesday");
-    break;
-  case 3:
-    console.log("wednesday");
-    break;
-  case 4:
-    console.log("thursday");
-    break;
-  case 5:
-    console.log("friday");
-    break;
-  case 6:
-    console.log("saturday");
-    break;
-  case 7:
-    console.log("sunday");
-    break;
+// let day = 5;
+// switch (day) {
+//   case 1:
+//     console.log("monday");
+//     break;
+//   case 2:
+//     console.log("tuesday");
+//     break;
+//   case 3:
+//     console.log("wednesday");
+//     break;
+//   case 4:
+//     console.log("thursday");
+//     break;
+//   case 5:
+//     console.log("friday");
+//     break;
+//   case 6:
+//     console.log("saturday");
+//     break;
+//   case 7:
+//     console.log("sunday");
+//     break;
 
-    default:
-        console.log("invalid day");
+//     default:
+//         console.log("invalid day");
         
+// }
+
+
+// question = WRite a program to find the current month using switch  statement!?
+
+//find the two output of two numbers base on switch using operator (+,-,*,/)?
+// wap to print action on trafic light using switch case
+
+let commnad = "yellow"
+switch(commnad){
+  case "red":
+  console.log("stop");
+  
+  break;
+  case "green":
+    console.log("go");
+    break;
+    case "yellow":
+      console.log("start you vechicle");
+      break;
+      
+    
+    default:
+      console.log("please wait untill trafic control");
+      break;
+      
+  
 }
