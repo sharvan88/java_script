336787efda0ca48ef2ec2b794ae0544678993c90
Sharvan <sharvan251120@gmail.com>
2025-09-04 14:07:32 +0530

    added string topic

--- day03/switch_case.js
+++ day03/switch_case.js
@@ -22,52 +22,71 @@ console.log("inavlid day")
 //     console.log("monday");
 //     break;
 //   case 2:
-//     console.log("tuesday");
-//     break;
-//   case 3:
-//     console.log("wednesday");
-//     break;
-//   case 4:
-//     console.log("thursday");
-//     break;
-//   case 5:
-//     console.log("friday");
-//     break;
-//   case 6:
-//     console.log("saturday");
-//     break;
-//   case 7:
-//     console.log("sunday");
-//     break;
+// //     console.log("tuesday");
+// //     break;
+// //   case 3:
+// //     console.log("wednesday");
+// //     break;
+// //   case 4:
+// //     console.log("thursday");
+// //     break;
+// //   case 5:
+// //     console.log("friday");
+// //     break;
+// //   case 6:
+// //     console.log("saturday");
+// //     break;
+// //   case 7:
+// //     console.log("sunday");
+// //     break;
 
-//     default:
-//         console.log("invalid day");
+// //     default:
+// //         console.log("invalid day");
         
-// }
+// // }
 
 
-// question = WRite a program to find the current month using switch  statement!?
+// // question = WRite a program to find the current month using switch  statement!?
 
-//find the two output of two numbers base on switch using operator (+,-,*,/)?
-// wap to print action on trafic light using switch case
+// //find the two output of two numbers base on switch using operator (+,-,*,/)?
+// // wap to print action on trafic light using switch case
 
-let commnad = "yellow"
-switch(commnad){
-  case "red":
-  console.log("stop");
+// let commnad = "yellow"
+// switch(commnad){
+//   case "red":
+//   console.log("stop");
   
-  break;
-  case "green":
-    console.log("go");
-    break;
-    case "yellow":
-      console.log("start you vechicle");
-      break;
+//   break;
+//   case "green":
+//     console.log("go");
+//     break;
+//     case "yellow":
+//       console.log("start you vechicle");
+//       break;
       
     
-    default:
-      console.log("please wait untill trafic control");
-      break;
+//     default:
+//       console.log("please wait untill trafic control");
+//       break;
       
   
+// }
+
+// wap to to find rearks based on grading system using switch statement
+
+let grade="A"
+switch (grade){
+    case "A":
+        console.log("excellent");
+        break;
+        case "B":
+            console.log("you are good student");
+            break;
+            case "C":
+                console.log("goot but need improvrment");
+                break;
+                deafult:
+                console.log("invalid grade");
+                break;          
+        
 }
---
+++ day04/strings.js
@@ -0,0 +1,17 @@
+// // today we will learn string
+// // methood 1
+// // let str1= "java_script"
+// // console.log(str1);
+
+// // methood 2
+// let str2= 'jAVA'
+// console.log(str2);
+
+
+// methood 3
+let name ="ashish";
+let age ="15";
+let city= "new delhi";
+console.log("my name is \n " + name," \n my age is\n " +age ," \n my city is \n"+city);
+
+// let str3='hello ${name}, welcome';
