bc5fe287b6ac287a73420a628495f9179857a446
Sharvan <sharvan251120@gmail.com>
2025-09-11 13:58:14 +0530

    added arrays

---
+++ day05/arrays.js
@@ -0,0 +1,16 @@
+ // ARRAY
+
+//  arrays are mutable that means we can change the array
+
+// how to declare a array
+
+
+// let arr =["ashish","sharvan","city=delhi"]
+// console.log(arr);
+
+
+// arrays are mix type of array that means we can add anything in array ex- function () "string" number, [1,2,2,5]=array 
+// k andar array bhi store kar sakte h 
+
+let array=["apple",2,{},[],function(){}, null , undefined]
+console.log(array);
