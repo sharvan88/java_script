c86d1e81be1260e1fa2ce6cde2e3d5a28af19856
Sharvan <sharvan251120@gmail.com>
2025-09-18 14:07:56 +0530

    added loops topic

---
+++ day07/loops.js
@@ -0,0 +1,47 @@
+// five types of loops
+/*
+no 1 = for loop
+no 2 = while loop
+no 3 = do while loop
+no 4 = for each loop
+no 5 = for off loop
+*/
+
+
+
+// for loop
+
+/*   for( intialize; condition; updation){
+            code execution
+}
+
+*/
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+    
+
+// }
+
+
+
+// while loop
+
+// let i=1;
+// while(i<=7){
+//     console.log(i);
+//     i++
+    
+// }
+
+
+
+// do while = it 
+let i=10;
+do{
+    console.log("ashish");
+    
+}while(i <=5)
+    i++
+   
+    
---
+++ day07/questions.js
@@ -0,0 +1,59 @@
+// print numbers from 0 to 10
+
+const res = require("express/lib/response");
+
+
+// for(i=0;i<=10;i++){
+//     console.log(i);
+    
+// }
+
+
+//print even numbers from 2 to 20
+// for(i=2; i<=20;i++){
+//      if(i%2==0)
+//     console.log("even numbers",i);
+      
+// }
+
+
+
+// print odd numbers from 1 to 19
+// for(i=1; i<=19;i++){
+//      if(i%2!==0)
+//     console.log("odd numbers",i);
+      
+// }
+
+// print numbers from 10 to 1 (reverse order)
+
+// for(i=10; i>=1; i--){ // i=10 than condition check i is greater than 10 true go to next line primt i
+//     console.log(i);
+    
+// }
+// }
+// print the first 10 multiples of 3
+
+// for(i=1; i<=10;i++){
+//     let num = 3;
+//     console.log(`3 * ${i} = ${num*i}`)
+// }
+// sum of numbers from 1 to 100
+
+let x=1;
+let y=1;
+let sum=0;
+for(let i=1; i<=100; i++)
+    x=y
+    y=z
+    sum=x+y
+    console.log(sum);
+    
+    
+
+
+    
+
+
+
+// print each characters of a string
