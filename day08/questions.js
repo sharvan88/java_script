79b3b400e9ac83fe292aecb8d1d9f998d8fdf1f0
Sharvan <sharvan251120@gmail.com>
2025-09-30 14:27:04 +0530

    function topic added in day09

---
+++ day08/questions.js
@@ -0,0 +1,39 @@
+// wap to find a cube of any numbers using any function
+// const cube=(num1)=>{
+//     return num1*
+// }
+// console.log(cube(4));
+//  wap a function to check if a number is odd or even
+
+// function checkoddoreven(number1){
+//     if(number1%2==0){
+//         console.log("your number is odd ");
+
+//     }else{
+//         console.log("number is even");
+
+//     }
+// }
+// checkoddoreven(2)
+// find the sum of all numbers in an array using function
+// const sumofarray = (arr) => {
+//   let sum = 0;
+//   for (i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum;
+// };
+// console.log(sumofarray([2, 7]));
+
+// count the numbers of vowels in a string using function
+const countofstr=(str)=>{
+    console.log(str.length);
+    
+
+return str;
+}
+console.log(countofstr("ashish"));
+
+// reverse a string without using built in methoods
+// Find the largest nuber in an array using function
+// Find the negative number in an array using function
