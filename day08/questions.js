9b0d39b4cbe2dba472a9f708822c429437471d0a
Sharvan <sharvan251120@gmail.com>
2025-10-01 14:04:24 +0530

    functions questions

--- day08/questions.js
+++ day08/questions.js
@@ -26,14 +26,52 @@
 // console.log(sumofarray([2, 7]));
 
 // count the numbers of vowels in a string using function
-const countofstr=(str)=>{
-    console.log(str.length);
-    
+// function countvowel(str){
+//     let vowel = "aeiouAEIOU"
+//     let count =0
+//     for(i=0; str.length;i++){
+//         if(vowel.includes(str[i]){
+// count++
+// }
 
-return str;
-}
-console.log(countofstr("ashish"));
-
+//     }
+// }
 // reverse a string without using built in methoods
+// function reversestring(str) {
+//     let reverse=""
+//   for (let i= str.length-1; i>=0; i--) {
+//     reverse+=str[i]
+//   }
+//   console.log(reverse);
+
+// }
+
+// reversestring("ashish");
+
 // Find the largest nuber in an array using function
+function largestnumber(arr) {
+  // let arr=[2,6,7]
+  let maxnum = [0]
+  for ( let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxnum) {
+      arr+=maxnum
+    }
+    console.log(maxnum);
+  }
+}
+largestnumber([5,8,10]);
 // Find the negative number in an array using function
+// Check if a string is a palindrone using function
+// Crete a function that returns the factorial of a number
+// create a function to count how many times a value appears in an array
+
+
+
+//  let arr=[2,6,7]
+//   let maxnum = [0];
+//   for (i = 1; i < arr.length; i++) {
+//     if (arr[i] > maxnum) {
+//       maxnum = arr[i];
+//     }
+//     console.log(maxnum);
+//   }
