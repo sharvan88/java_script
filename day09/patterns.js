7c058d292d39a895cec5ac7517a1e39c756dea47
Sharvan <sharvan251120@gmail.com>
2025-10-15 14:15:20 +0530

    hollow pattern solve

--- day09/patterns.js
+++ day09/patterns.js
@@ -84,19 +84,46 @@
 
 */
 
-let rows=5;
-for(let i=1; i<=rows; i++){
-  let spaces ='   '.repeat(rows-i);
-  let stars  =' * '.repeat(2*i-1);
-  console.log(spaces+stars);
+// let rows=5;
+// for(let i=1; i<=rows; i++){
+//   let spaces ='   '.repeat(rows-i);
+//   let stars  =' * '.repeat(2*i-1);
+//   console.log(spaces+stars);
   
 
-}
+// }
 
-for(let i=rows-1; i>=1; i--){
-  let spaces ='   '.repeat(rows-i);
-  let stars  =' * '.repeat(2*i-1);
-  console.log(spaces+stars);
+// for(let i=rows-1; i>=1; i--){
+//   let spaces ='   '.repeat(rows-i);
+//   let stars  =' * '.repeat(2*i-1);
+//   console.log(spaces+stars);
   
-}
+// }
+
+// hollow pattern 
+
+/*
+*****
+*   *
+*   *
+*   *
+*****
+*/
+
+// let rows=5;
+// let col =5;
+// for(let i=1; i<=rows; i++){
+//   let line="";
+//   for(let j=1; j<=col; j++){
+//     if(i===1 || i===rows  || j===1 || j===col){
+//       line+="*";
+//     }else{
+//       line+=" ";
+//     }
+    
+
+//   }
+//   console.log(line);
+  
+// }
 
