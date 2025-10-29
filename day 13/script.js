6fdcc2f4e61ec40e6d866c6e8d8e7a1c9e14bc79
Sharvan <sharvan251120@gmail.com>
2025-10-29 14:24:58 +0530

    dynamic div created using dom

---
+++ day 13/script.js
@@ -0,0 +1,13 @@
+const div=document.createElement("div")
+
+div.style.backgroundColor = 'salmon';
+div.style.color = 'white';
+div.style.width = '100%';
+div.style.height = '20px';
+div.textContent = 'hey i am a div';
+div.style.textAlign="center"
+
+
+console.log(div);
+
+document.body.appendChild(div)
